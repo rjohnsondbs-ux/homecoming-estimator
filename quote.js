d3fb52142a3fe6c38eab351897a1ca0d12534368
rjohnsondbs-ux <rjohnsondbs@gmail.com>
2026-06-15 00:22:08 -0500

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,3 +1,12 @@
+export const config = {
+  maxDuration: 60,
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb'
+    }
+  }
+};
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
